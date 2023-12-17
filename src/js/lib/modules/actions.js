14b74ad6ec6c7611739c740bf11973c7fccdf4fe
Kramarenko Maxim <kramarenko-90@inbox.ru>
2023-12-18 02:03:27 +0300

    methods part 1

--- src/js/lib/modules/actions.js
+++ src/js/lib/modules/actions.js
@@ -12,8 +12,10 @@ $.prototype.html = function(content) {
     return this;
 };
 
+// получение элемента по номеру
+
 $.prototype.eq = function(i) { 
-    console.log(this);
+    // console.log(this);
     const swap = this[i]; // вытаскиваем элемент из большого объекта
     const objLength = Object.keys(this).length; // мы превращаем объект в массив, который формируется из свойств объекта, включая свойство length и другие свойства внутри core.js
     
@@ -26,6 +28,52 @@ $.prototype.eq = function(i) {
 
     this[0] = swap; // приписывает первому элементу объекта объект, который вытащили
     this.length = 1; // обновляем свойство length
+
+    return this;
+};
+
+// получение номера элмента
+
+$.prototype.index = function () {
+    const parent = this[0].parentNode; // получили родителя искомого элемента
+    const childs = [...parent.children]; // получаем всех потомков, но получаем html коллекцию (псевдомассив), поэтому мы помещаем в квадратные скобки и используем спред оператор
+
+    const findMyIndex = (item) => {
+        return item == this[0];
+    };
+
+    return childs.findIndex(findMyIndex);
+    // по синтаксису метода мы должны передать callback функцию с кретерием отбора
+};
+
+// получение элемента среди определённых
+
+$.prototype.find = function(selector) {
+    let numberOfItems = 0; // колическтво элементов, которые подошли по этому селектору
+    let counter = 0; // количество записанных элементов в обновлённый this 
+
+    const copyObj = Object.assign({}, this); // поверхностная копия объекта this
+
+    for (let i = 0; i < copyObj.length; i++) {
+        const arr = copyObj[i].querySelectorAll(selector); // это массив, состоящий из html элементов из объекта подходящих по селектору
+        if (arr.length == 0) {
+            continue;
+        }
+
+        for (let j = 0; j < arr.length; j++) {
+            this[counter] = arr[j];
+            counter++;
+        }
+
+        numberOfItems += arr.length;
+    }
+
+    this.length = numberOfItems;
+
+    const objLength = Object.keys(this).length;
+    for(; numberOfItems < objLength; numberOfItems++) {
+        delete this[numberOfItems]; // 3 элемента из 5 подходят, 3 элмент имеет порядковый номер 2
+    }
 
     return this;
 };
